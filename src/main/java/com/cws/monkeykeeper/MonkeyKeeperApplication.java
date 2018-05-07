6da077246ead79d4d3ff406bb547558ca2dd29c3
buivancuong <bvcuongit@gmail.com>
2018-05-07 11:38:44 +0700

    First commit

---
+++ src/main/java/com/cws/monkeykeeper/MonkeyKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.cws.monkeykeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyKeeperApplication.class, args);
+	}
+}
