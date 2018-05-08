5d7e22b26ae440a6b891f5527083246fa68b755c
buivancuong <bvcuongit@gmail.com>
2018-05-08 10:09:03 +0700

    123

--- src/main/java/com/cws/monkeykeeper/MonkeyKeeperApplication.java
+++ src/main/java/com/cws/monkeykeeper/MonkeyKeeperApplication.java
@@ -2,10 +2,25 @@ package com.cws.monkeykeeper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
-public class MonkeyKeeperApplication {
-
+@Configuration
+@ComponentScan(basePackages ="com.cws.monkeykeeper")
+public class MonkeyKeeperApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MonkeyKeeperApplication.class);
+	}
+	@Bean
+	protected static PropertySourcesPlaceholderConfigurer propertyConfiInDev() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MonkeyKeeperApplication.class, args);
 	}
